5f5e8f24cab3916a57b10e40f7ed32b20cfe2206
turb <turb@pisscake.com>
2015-05-05 15:49:27 -0700

    now with svgs and some material design

--- adv.py
+++ adv.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python
 import json
 
-htmlTemplate = """
+pageTemplate = """
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="adv.css">
+	<link rel="stylesheet" type="text/css" href="adv.css">
 </head>
 <body>
-<div>test</div>
+	<div id="contentContainer">
+		<div id="content">
+			%s
+			<div id="addOption">
+				<div id="addOptionButton">
+					<div id="addIcon" class="addOptionButtonIcon"></div>
+					<div id="editIcon" class="addOptionButtonIcon"></div>
+					<div id="sendIcon" class="addOptionButtonIcon"></div>
+				</div>
+				<div id="addOptionTextBox">add your own choice</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
+"""
+
+storyTemplate = """
+<div id="storyBox">
+	%s
+</div>
+"""
+
+optionTemplate = """
+<div class="optionBox">
+	%s
+</div>
 """
 
 def printScene(scene):
@@ -35,11 +59,18 @@ def application(environ, start_response):
 
 	start_response('200 OK', [('Content-Type','text/html')])
 	#return [b'Hello World']
-	return [str.encode(htmlTemplate)]
+	return [
+		str.encode(
+			pageTemplate % (
+				(storyTemplate % "something happens...<br/>possibly long<br/>with many lines<br/>...<br/>...<br/>yep")
+				+ (optionTemplate % "do something")
+				+ (optionTemplate % "do something else")
+				+ (optionTemplate % "do yet another thing")))
+		]
 
 	# build the response body possibly using the environ dictionary
 	#response_body = 'The request method was %s' % environ['REQUEST_METHOD']
-	
+
 	# HTTP response code and message
 	#status = '200 OK'
 
